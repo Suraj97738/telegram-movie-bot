b6d6a51b5cafdf5d3909102267126860accea0f0
Suraj97738 <152477023+Suraj97738@users.noreply.github.com>
2026-01-15 09:32:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,11 @@
 import os
-from pyrogram import Client, filters
-from motor.motor_asyncio import AsyncIOMotorClient
+from pyrogram import Client
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-MONGO_URI = os.environ.get("MONGO_URI")
+print("BOT STARTING...")
 
-mongo = AsyncIOMotorClient(MONGO_URI)
-db = mongo.moviebot.files
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 app = Client(
     "moviebot",
@@ -16,11 +13,5 @@ app = Client(
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
-
-@app.on_message(filters.command("start"))
-async def start(_, msg):
-    await msg.reply_text(
-        "🎬 Movie Bot Online!\n\nSend movie name to search."
-    )
 
 app.run()
