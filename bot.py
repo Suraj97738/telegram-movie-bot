355126630fd75d9c20ddf4c2edbc3c2f0116554d
Suraj97738 <152477023+Suraj97738@users.noreply.github.com>
2026-01-15 09:34:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
 import os
-from pyrogram import Client
-
-print("BOT STARTING...")
+from pyrogram import Client, filters
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
@@ -13,5 +11,9 @@ app = Client(
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
+
+@app.on_message(filters.command("start"))
+async def start(_, message):
+    message.reply_text("🎬 Movie Bot Online!")
 
 app.run()
